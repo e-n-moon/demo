8c048f8dddec06dc6eb19f4dd101dccb30d463eb
Evana Moon <evan.moon@icloud.com>
2026-06-18 04:50:51 +1000

    Update Supabase URL and anon key

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "YOUR_PROJECT_URL";
-const SUPABASE_ANON_KEY = "YOUR_ANON_KEY";
+const SUPABASE_URL = "https://qmkgigdpvsxgiiafitxb.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFta2dpZ2RwdnN4Z2lpYWZpdHhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExOTk4NzEsImV4cCI6MjA5Njc3NTg3MX0.drvWrE0ELimT6-IAzn6fzeeXD2tQZ138BvHODDfyHsQ";
 
 const supabaseClient =
   supabase.createClient(
