a08b6cb22abbd666e35a8728eaa6469c68b76b35
Evana Moon <evan.moon@icloud.com>
2026-06-18 05:17:59 +1000

    Refactor supabase client initialization

--- supabase.js
+++ supabase.js
@@ -1,8 +1,6 @@
 const SUPABASE_URL = "https://qmkgigdpvsxgiiafitxb.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFta2dpZ2RwdnN4Z2lpYWZpdHhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExOTk4NzEsImV4cCI6MjA5Njc3NTg3MX0.drvWrE0ELimT6-IAzn6fzeeXD2tQZ138BvHODDfyHsQ";
 
-const supabaseClient =
-  supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-  );
+window.sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+console.log("✔ supabase initialized");
